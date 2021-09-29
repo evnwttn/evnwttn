c2425b0ef1638ecf9969428a00a52a38a7366e7b
evnwttn <evnwttn@gmail.com>
2021-09-29 12:22:48 -0230

    removed icons / resizes

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const open = document.querySelector("#splash");
-const container = document.querySelector("#yo");
-
-// open.addEventListener("click", (event) => {
-//   open.classList.add("invisible");
-//   container.classList.remove("invisible");
-// });
-
 let animation = anime({
   targets: "#bar",
   height: "0%",
